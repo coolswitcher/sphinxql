066d674559bbc65cb28c553d5e20424f57bbc0ad
coolswitcher <CoolSwitcher@gmail.com>
2020-04-08 16:03:31 +0300

    Refactoring
    add replace, delete methods

--- src/SphinxQL.php
+++ src/SphinxQL.php
@@ -4,369 +4,454 @@
  *
  * How to use:
  * $db = \AppZz\DB\SphinxQL::factory ()
- * 				->select('*')
- * 				->from('index')
+ * 				->select('index', ['id', 'title'])
  * 				->match('@field', 'foobar')
  * 				->where('field2', '>', 2)
  * 				->between('field3', 10, 90)
- * 				->execute();
+ * 				->execute()
+ * 				->as_array();
  */
 namespace AppZz\DB;
 
 /**
  * @package SphinxQL
- * @version 1.0.0
+ * @version 1.2.0
  * @author CoolSwitcher
  * @license MIT
  */
 class SphinxQL {
 
-	private $sphinxql    = NULL;
-	private $debug       = FALSE;
-	private $opts        = array ();
-	private $where       = array ();
-	private $stats       = array ();
+	private $_db    = NULL;
+	private $_debug = FALSE;
+	private $_opts  = array ();
+	private $_where = array ();
+	private $_stats = array ();
+	private $_query = 'SELECT';
+	private $_index;
+	private $_fields;
+	private $_fields_array;
+	private $_values;
+	private $_result;
 
-	private $default_opts = array (
-		'limit'       =>20,
-		'offset'      =>0,
-		'pairs'       => FALSE,
-		'max_matches' =>1000,
+	const SELECT    = 'SELECT';
+	const UPDATE    = 'UPDATE';
+	const INSERT    = 'INSERT';
+	const REPLACE   = 'REPLACE';
+	const DELETE    = 'DELETE';
+
+	private $_default_opts = array (
+		'limit'       => 20,
+		'offset'      => 0,
+		'max_matches' => 1000,
 	);
 
-	public function __construct ( $host = '127.0.0.1', $port = 9306, $debug = FALSE ) {
-	    $this->sphinxql = mysqli_init();
-	    if ( !$this->sphinxql )
-	    	$this->error ('MySQLi not initialed');
-		if ( !@mysqli_real_connect ( $this->sphinxql, $host, NULL, NULL, NULL, $port, NULL ) )
-			$this->error ('Can\'t connect to SphinxQL: ' . mysqli_connect_errno() . ' ' . mysqli_connect_error() );
-		$this->opts  = $this->default_opts;
-		$this->debug = (bool) $debug;
-	}
+	public function __construct ($host = '127.0.0.1', $port = 9306, $debug = FALSE)
+	{
+	    $this->_db = mysqli_init();
 
-	public static function factory ( $host = '127.0.0.1', $port = 9306, $debug = FALSE ) {
-		return new SphinxQL ( $host, $port, $debug );
-	}
+	    if ( ! $this->_db ) {
+	    	$this->_error ('MySQLi not initialed');
+	    }
 
-	public function select ( $fields = '*' ) {
-		if ( empty ($fields) )
-			$this->error ('Fields are empty');
-		$this->opts['fields'] = $fields;
-		if ( strpos ($fields, ',') AND $fields != '*' ) {
-			$fields_array = explode (',', $fields);
-			$fields_array = array_map ('trim', $fields_array);
-			$this->opts['fields_array'] = $fields_array;
+		if ( ! mysqli_real_connect ( $this->_db, $host, NULL, NULL, NULL, $port, NULL ) ) {
+			$this->_error ('Can\'t connect to SphinxQL: ' . mysqli_connect_errno() . ' ' . mysqli_connect_error() );
 		}
+
+		$this->_opts   = $this->_default_opts;
+		$this->_debug = (bool) $debug;
+	}
+
+	public static function factory ($host = '127.0.0.1', $port = 9306, $debug = FALSE)
+	{
+		return new SphinxQL ($host, $port, $debug);
+	}
+
+	public function fields ($fields = '*')
+	{
+		if ( ! empty ($fields)) {
+			if (is_array($fields)) {
+				$this->_fields = implode (', ', $fields);
+			} else {
+				$this->_fields = $fields;
+			}
+		}
+	}
+
+	public function select ($index = 'index', $fields = '*')
+	{
+		$this->_query = SphinxQL::SELECT;
+		$this->fields($fields);
+		$this->index($index);
 		return $this;
 	}
 
-	public function from ( $index = 'index' ) {
-		if ( empty ($index) )
-			$this->error ('Index are empty');		
-		$this->opts['index'] = $index;
+	public function insert ($index = 'index', array $fields = array ())
+	{
+		$this->_query = SphinxQL::INSERT;
+		$this->fields($fields);
+		$this->index($index);
 		return $this;
 	}
 
-	public function match ($field, $values, $exact_match = FALSE) {
+	public function replace ($index = 'index', array $fields = array ())
+	{
+		$this->_query = SphinxQL::REPLACE;
+		$this->fields($fields);
+		$this->index($index);
+		return $this;
+	}
+
+	public function delete ($index = 'index')
+	{
+		$this->_query = SphinxQL::DELETE;
+		$this->index($index);
+		return $this;
+	}
+
+	public function index ($index = 'index')
+	{
+		if (empty ($index)) {
+			$this->_error ('Index are empty');
+		}
+
+		$this->_index = $index;
+		return $this;
+	}
+
+	public function values (array $values = array())
+	{
+		if (empty ($values)) {
+			$this->_error ('Values are empty');
+		}
+
+		$this->_values = $values;
+		return $this;
+	}
+
+	public function match ($field, $values, $exact_match = FALSE)
+	{
 		$this->_where ($field, $values, 'match', $exact_match);
 		return $this;
 	}
 
-	public function where ($field, $cmp, $values) {
+	public function where ($field, $cmp, $values)
+	{
 		$this->_where ($field, $values, 'default', $cmp);
 		return $this;
 	}
 
-	public function between ($field, $min, $max) {
-		if ( $min >= $max )
-			$this->error('min may be smalller than max');
+	public function between ($field, $min, $max)
+	{
+		if ($min >= $max) {
+			$this->_error('min may be smalller than max');
+		}
+
 		$this->_where ($field, array ($min, $max), 'between');
 		return $this;
 	}
 
-	public function groupby ($field) {
-		$this->opts['groupby'] = $field;
+	public function groupby ($field)
+	{
+		$this->_opts['groupby'] = $field;
 		return $this;
 	}
 
-	public function orderby ($field, $asc = 'asc') {
-		$this->opts['orderby'][$field] = strtoupper($asc);
+	public function orderby ($field, $asc = 'asc')
+	{
+		$asc = strtoupper ($asc);
+		$this->_opts['orderby'] = $field;
+		$this->_opts['order']   = in_array($asc, array('ASC','DESC')) ? $asc : 'ASC';
 		return $this;
-	}	
+	}
 
-	public function limit ($offset = 0, $limit = 0) {
- 		if ( $offset !== 0 AND $limit === 0 ) {
-			$this->opts['offset'] = 0;
-			$this->opts['limit']  = $offset;			
+	public function limit ($offset = 0, $limit = 0)
+	{
+ 		if ($offset !== 0 AND $limit === 0) {
+			$this->_opts['offset'] = 0;
+			$this->_opts['limit']  = $offset;
  		}
 		else {
-			$this->opts['offset'] = $offset;
-			$this->opts['limit']  = $limit;			
-		} 
-		return $this;
-	}
-
-	public function max_matches ($max = 1000) {
-		$this->opts['max_matches'] = intval ($max);
-		return $this;
-	}	
-
-	public function pairs ($pairs = TRUE) {
-		$this->opts['pairs'] = (bool) $pairs;
-		return $this;
-	}		
-
-	public function has_query () {
-		return (bool) !empty ($this->where);
-	}
-
-	public function rawQuery ( $query ) {
-		if ($this->debug)
-			$start = microtime(TRUE);
-
-		$result = $this->sphinxql->query ($query);
-
-		if ($this->debug) {
-			$timer = microtime(TRUE) - $start;			
-			$this->stats[] = array(
-				'query' => $query,
-				'start' => $start,
-				'timer' => $timer,
-			);					
-		}	
-
-		if ( $result !== FALSE ) {
-			$return = array ();
-			if ( $result->num_rows > 0 ) {
-		        while ( $row = $result->fetch_assoc() ) {	 
-		        	$return[] = $row;	        		
-		        }				
-			}
-	        $result->free();
-	        if ($this->debug)
-	        	$this->cutStats();
-	        return $return;	        			
-		} else {
-			$error = $this->sphinxql->error;	
-			if ($this->debug) {
-				end($this->stats);
-				$key = key($this->stats);
-				$this->stats[$key]['error'] = $error;
-				$this->cutStats();							
-			}		
-			$this->error("{$error}. Full query: [{$query}]");	
+			$this->_opts['offset'] = $offset;
+			$this->_opts['limit']  = $limit;
 		}
-		return FALSE;				
+
+		return $this;
 	}
 
-	public function execute () {
-		$where = $this->_buildWhere();
-		$order = $this->_buildGroupOrder();
-		if ( empty ($where) )
-			$this->error ('Where conditions are empty');
-		$query = sprintf ("SELECT %s, WEIGHT() as weight FROM %s WHERE %s%s LIMIT %d, %d option max_matches=%d", $this->opts['fields'], $this->opts['index'], $where, $order, $this->opts['offset'], $this->opts['limit'], $this->opts['max_matches']);
-		
-		if ($this->debug)
+	public function max_matches ($max = 1000)
+	{
+		$this->_opts['max_matches'] = intval ($max);
+		return $this;
+	}
+
+	public function execute ()
+	{
+		$where = $this->_build_where();
+		$order = $this->_build_group_order();
+
+		if ($this->_query === SphinxQL::SELECT AND $this->_query !== SphinxQL::DELETE) {
+			if (empty ($where)) {
+				$this->_error ('Where conditions are empty');
+			}
+		}
+
+		switch ($this->_query) {
+			case SphinxQL::SELECT:
+					if (empty ($this->_fields)) {
+						$this->_error ('Fields are empty');
+					}
+
+					$query = sprintf ("SELECT %s, WEIGHT() as weight FROM %s WHERE %s%s LIMIT %d, %d option max_matches=%d", $this->_fields, $this->_index, $where, $order, $this->_opts['offset'], $this->_opts['limit'], $this->_opts['max_matches']);
+				break;
+
+			case SphinxQL::INSERT:
+			case SphinxQL::REPLACE:
+				$values = $this->_build_values();
+				$query  = sprintf ("%s INTO %s %s", $this->_query, $this->_index, $values);
+				break;
+
+			case SphinxQL::DELETE:
+				$query = sprintf ("DELETE FROM %s WHERE %s", $this->_index, $where);
+				break;
+		}
+
+		if ($this->_debug) {
 			$start = microtime(TRUE);
+		}
 
-		$result = $this->sphinxql->query ($query);
+		$this->_result = $this->_db->query ($query);
 
-		if ($this->debug) {
-			$timer = microtime(TRUE) - $start;			
-			$this->stats[] = array(
+		if ($this->_debug) {
+			$timer = microtime(TRUE) - $start;
+			$this->_stats[] = array(
 				'query' => $query,
 				'start' => $start,
 				'timer' => $timer,
-			);					
-		}		
+			);
+		}
 
-	    if ( $result !== FALSE ) {
-	    	$return = array ();
-	    	if ( !isset ($this->opts['fields_array']) AND ($this->opts['fields'] != '*') )
-	    		$sp_result = 'column';
-        	elseif ( sizeof ($this->opts['fields_array']) === 2 AND $this->opts['pairs'] !== FALSE ) {
-				$sp_result = 'pairs';
-				$sp_key = array_shift($this->opts['fields_array']);
-				$sp_value = array_pop($this->opts['fields_array']);	
+		if ($this->_result !== FALSE) {
+			if ($this->_query !== SphinxQL::SELECT) {
+				return $this->_db->affected_rows;
 			} else {
-				$sp_result = 'row';
+				return ($this->_result !== FALSE);
 			}
+		}
 
-	        while ( $row = $result->fetch_assoc() ) {	 
-	        	switch ($sp_result) {
-	        		case 'column':
-	        			$return[] = $row[$this->opts['fields']];
-	        		break;
-	        		case 'pairs':	
-	        			$return[$row[$sp_key]] = $row[$sp_value];
-	        		break;
-	        		default:	
-	        			$return[] = $row;	        		
-	        		break;	
-	        	}           	
-	        }
-	        $result->free();
-	        if ($this->debug)
-	        	$this->cutStats();
-	       	$this->flushOpts();
-	        return $return;
-	    } else {
-			$error = $this->sphinxql->error;	
-			if ($this->debug) {
-				end($this->stats);
-				$key = key($this->stats);
-				$this->stats[$key]['error'] = $error;
-				$this->cutStats();							
-			}		
-			$this->error("{$error}. Full query: [{$query}]");	
-	    }
-	   	$this->flushOpts();
-	    return false;		
+		return FALSE;
 	}
 
-	public function getStats() {
-		return $this->stats;
-	}	
+	public function as_array ($id = NULL, $name = NULL)
+	{
+	    $return = array ();
 
-	public function lastQuery() {
-		$last = end($this->stats);
+	    if ($this->_result !== FALSE AND $this->_query === SphinxQL::SELECT) {
+	        while ($row = $this->_result->fetch_assoc()) {
+	        	if (empty($id) AND empty($name)) {
+	        		$return[] = $row;
+	        	} else {
+	        		if ( ! empty ($name)) {
+	        			if (empty($id)) {
+	        				$return[] = $row[$name];
+	        			} else {
+	        				$return[$row[$id]] = $row[$name];
+	        			}
+	        		}
+	        	}
+	        }
+
+		    $this->_result->free();
+
+	        if ($this->_debug) {
+	        	$this->_cut_stats();
+	        }
+
+	       	$this->_flush_opts();
+	    }
+
+	    return $return;
+	}
+
+	public function get_stats()
+	{
+		return $this->_stats;
+	}
+
+	public function last_query ()
+	{
+		$last = end($this->_stats);
 		return isset ($last['query']) ? $last['query'] : FALSE;
-	}	
+	}
 
-	private function flushOpts () {
-		$this->opts  = $this->default_opts;
+	private function _flush_opts ()
+	{
+		$this->_opts = $this->_default_opts;
 		$this->where = array ();
 	}
 
-	private function _where ($field, $values, $type, $extra = NULL) {
-		$field = $this->_checkField ($field);
+	private function _where ($field, $values, $type, $extra = NULL)
+	{
+		$field = $this->_check_field ($field);
 
-		if ( is_array ($values) AND (!empty ($values)) AND ($extra === 'in') ) {
+		if (is_array ($values) AND (!empty ($values)) AND ($extra === 'in')) {
 			$type = 'in';
 			$extra = NULL;
 		}
 
 		$this->where[] = array ('field'=>$field, 'values'=>$values, 'type'=>$type, 'prefix'=>$this->_where_prefix(), 'extra'=>$extra);
-	}		
+	}
 
-	private function _where_prefix () {
+	private function _where_prefix ()
+	{
 		return !empty ( $this->where ) ? 'AND' : '';
 	}
 
-	public function _escape_string ( $string ) {
+	public function _escape_string ($string)
+	{
 	    $from = array ( '\\', '(',')','|','-','!','@','~','"','&', '/', '^', '$', '=', "'", "\x00", "\n", "\r", "\x1a" );
 	    $to   = array ( '\\\\', '\\\(','\\\)','\\\|','\\\-','\\\!','\\\@','\\\~','\\\"', '\\\&', '\\\/', '\\\^', '\\\$', '\\\=', "\\'", "\\x00", "\\n", "\\r", "\\x1a" );
-	    return str_replace ( $from, $to, $string );
-	}	
+	    return str_replace ($from, $to, $string);
+	}
 
-	private function _escape ( $value ) {
+	private function _escape ($value)
+	{
 		$value = trim ($value);
-		if ( !is_numeric($value) && !is_null ($value) ) {
+
+		if ( ! is_numeric($value) AND ! is_null ($value)) {
 			$value = "'" . $this->_escape_string ($value) . "'";
 		}
+
 		return $value;
 	}
 
-	private function _escapeMatch ( $value, $exact_match = FALSE ) {
+	private function _escape_match ($value, $exact_match = FALSE)
+	{
 		$value = (array) $value;
 		$obj = $this;
+
 		$value = array_map (function ($v) use ($exact_match, $obj) {
 			$v = trim ($v);
 			$v = $obj->_escape_string ($v);
-			if ($exact_match === -1)
+			if ($exact_match === -1) {
 				$v = '"^' . $v . '"';
-			else
+			} else {
 				$v = $exact_match ? '"^' . $v . '$"' : '"' . $v . '"';
+			}
+
 			return $v;
 		}, $value);
+
 		return implode (' | ', $value);
 	}
 
-	private function _escapeValues ( $values ) {
-		if ( !is_array ($values) ) 
+	private function _escape_values ($values)
+	{
+		if ( ! is_array ($values)) {
 			$values = array ($values);
-		$values = array_map( array ($this, '_escape'), $values);	 
+		}
+
+		$values = array_map (array ($this, '_escape'), $values);
 		return $values;
-	}	
+	}
 
-	private function _checkField ( $field ) {
+	private function _check_field ($field)
+	{
 		$field = preg_replace ('#[^a-z_,@\(\)]+#iu', '', $field);
-		if ( empty ($field) )
-			$this->error ('Wrong field name');
-		return trim ($field);		
-	}	
 
-	private function _buildWhere () {
+		if (empty ($field)) {
+			$this->_error ('Wrong field name');
+		}
+
+		return trim ($field);
+	}
+
+	private function _build_values ()
+	{
+		$values = $this->_escape_values ($this->_values);
+		return sprintf ("(%s) VALUES (%s)", $this->_fields, implode (', ', array_values($values)));
+	}
+
+	private function _build_where ()
+	{
 		$where = '';
 		$matches = '';
-		if ( !empty ($this->where) ) {
+
+		if ( ! empty ($this->where)) {
 			foreach ($this->where as &$param) {
-				if ($param['type'] === 'match')
-					$values = $this->_escapeMatch($param['values'], $param['extra']);
-				else
-					$values = $this->_escapeValues($param['values']);
-				switch ( $param['type'] ) {
+				if ($param['type'] === 'match') {
+					$values = $this->_escape_match($param['values'], $param['extra']);
+				} else {
+					$values = $this->_escape_values($param['values']);
+				}
+
+				switch ($param['type']) {
 					case 'between':
 						  $where .= ( !empty ($param['prefix']) ? $param['prefix'] . ' ' : ' ' ) . $param['field'] . ' BETWEEN ' . $values[0] . ' AND ' . $values[1] . ' ';
 					break;
 					case 'match':
 						$matches .= $param['field'] . ' ' . $values . ' ';
-					break;	
+					break;
 					case 'in':
 						$where .= ( !empty ($param['prefix']) ? $param['prefix'] . ' ' : '' ) . $param['field'] . ' IN (' . implode (', ', $values) . ') ';
-					break;				
+					break;
 					default:
 						  $where .= ( !empty ($param['prefix']) ? $param['prefix'] . ' ' : '' ) . $param['field'] . ' ' . $param['extra'] . ' ' . $values[0] . ' ';
-					break;							  
-				}				
+					break;
+				}
 			}
-			if ( !empty ($matches) ) {
+
+			if ( ! empty ($matches) ) {
 				$matches = 'MATCH (\'' . trim ($matches) . '\')';
-				if ( !empty ($where) )
+
+				if ( ! empty ($where)) {
 					$where = $matches . ' ' . $where;
-				else
-					$where = $matches; 
+				} else {
+					$where = $matches;
+				}
 			}
 		}
+
 		return trim ($where);
 	}
 
-	private function _buildGroupOrder () {
+	private function _build_group_order ()
+	{
 		$group_order = '';
-		if ( isset ($this->opts['groupby']) AND !empty ($this->opts['groupby']) ) 
-			$group_order = 'GROUP BY ' . $this->opts['groupby'];
-		if ( isset ($this->opts['orderby']) AND !empty ($this->opts['orderby']) ) {
-			$tot_orders = sizeof($this->opts['orderby']);
-			$ord_cnt    = 0; 
-			$group_order .= ' ORDER BY ';
-			foreach ($this->opts['orderby'] as $field=>&$asc) {
-				$ord_cnt++;
-				$group_order .= $field . ' ' . $asc;
-				if ( $ord_cnt < $tot_orders )
-					$group_order .= ', '; 
-			}
+
+		if ( ! empty ($this->_opts['groupby']))
+		{
+			$group_order = sprintf (' GROUP BY %s', $this->_opts['groupby']);
 		}
+
+		if ( ! empty ($this->_opts['orderby']) AND ! empty ($this->_opts['order']))
+		{
+			$group_order .= sprintf (' ORDER BY %s %s', $this->_opts['orderby'], $this->_opts['order']);
+		}
+
 		return ' ' . trim ($group_order);
 	}
 
-	private function cutStats() {
-		if ( sizeof($this->stats) > 100 ) {
-			reset($this->stats);
-			$first = key($this->stats);
-			unset($this->stats[$first]);
+	private function _cut_stats ()
+	{
+		if (sizeof($this->_stats) > 100) {
+			reset($this->_stats);
+			$first = key($this->_stats);
+			unset($this->_stats[$first]);
 		}
-	}	
-
-	private function error ( $err ) {
-		throw new \Exception ($err);
 	}
 
-	public function close () {
-		$this->sphinxql->close();
+	private function _error ($message)
+	{
+		throw new \Exception ($message);
 	}
 
-	public function __destroy () {
+	public function close ()
+	{
+		$this->_db->close();
+	}
+
+	public function __destroy ()
+	{
 		$this->close();
 	}
-
 }
